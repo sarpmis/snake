10d52c589b8e241e1dfcaddde3555f28fbbe98f4
Sarp Misoglu <sarp.misoglu@pomona.edu>
2017-12-03 23:04:05 -0800

    four

--- main.c
+++ main.c
@@ -11,89 +11,51 @@
 
 
 void main(void) {
+	// thread to handle user inputs
+	pthread_t thread1;
+	pthread_create(&thread1, NULL, t1, (void*) &snakeDirection);
+	pthread_join(thread1, NULL);
+	
+	// main thread runs the game
 	pioInit();
 	spiInit(244000, 0);
 
-	// char board[64];
+	char board[64];
 
-	// // testRow(board, 0);
+	// testRow(board, 0);
 
-	// int y = 0;
-	// for(y; y < 8; y++){
-	// 	addToBoard(board, y, 0, 7);
-	// }
-	// drawBoard(board);
-	// delayMillis(1000);	
+	int y = 0;
+	for(y; y < 8; y++){
+		addToBoard(board, y, 0, 7);
+	}
+	drawBoard(board);
+	delayMillis(1000);	
 
-	// clearBoard(board);
+	clearBoard(board);
 	
-	// head = (struct SnakeNode*)malloc(sizeof(struct SnakeNode));
-	// head->z = 7;
-	// head->y = 0;
-	// head->x = 4;
+	head = (struct SnakeNode*)malloc(sizeof(struct SnakeNode));
+	head->z = 7;
+	head->y = 0;
+	head->x = 4;
 
-	// struct SnakeNode* temp = (struct SnakeNode*)malloc(sizeof(struct SnakeNode));
-	// temp->z = 7;
-	// temp->y = 0;
-	// temp->x = 3;
-	// head->prev = temp;
-	// addSnake(board);
-	// drawBoard(board);
+	struct SnakeNode* temp = (struct SnakeNode*)malloc(sizeof(struct SnakeNode));
+	temp->z = 7;
+	temp->y = 0;
+	temp->x = 3;
+	head->prev = temp;
+	addSnake(board);
+	drawBoard(board);
 
-	// delayMillis(1000);
+	snakeDirection = POSX;
 
-	// snakeDirection = POSX;
-
-	// insertHead();
-	// clearBoard(board);
-	// addSnake(board);
-	// drawBoard(board);
-
-	// delayMillis(1000);
-	// insertHead();
-	// clearBoard(board);
-	// addSnake(board);
-	// drawBoard(board);
-
-	// delayMillis(1000);
-	// removeTail();
-	// clearBoard(board);
-	// addSnake(board);
-	// drawBoard(board);
-
-	// delayMillis(1000);
-	// removeTail();
-	// clearBoard(board);
-	// addSnake(board);
-	// drawBoard(board);
-	
-	// delayMillis(1000);
-	// removeTail();
-	// clearBoard(board);
-	// addSnake(board);
-	// drawBoard(board);
-
-	// delayMillis(1000);
-	// removeTail();
-	// clearBoard(board);
-	// addSnake(board);
-	// drawBoard(board);
-
-	// printf("done\n");
-
-	// char c;
-	// printf("(getche example) please type a letter: ");
-	// c = getche();
-	// printf("\nYou typed: %c\n", c);
-	// printf("(getch example) please type a letter...");
-	// c = getch();
-	// printf("\nYou typed: %c\n", c);
-
-	pthread_t thread1;
-	char *message1 = "thread 1";
-
-	pthread_create(&thread1, NULL, t1, (void*) message1);
-	pthread_join(thread1, NULL);
-	printf("all done!\n");
+	while(1) {
+		delayMillis(1000);
+		insertHead();
+		removeTail();
+		clearBoard(board);
+		addSnake(board);
+		drawBoard(board);
+	}
+	printf("done\n");
 }
 
--- user_inputs.h
+++ user_inputs.h
@@ -1,7 +1,7 @@
 
 void *t1(void *ptr){
-	char *message;
-	message = (char *)ptr;
+	int *snake_dir;
+	snake_dir = (int *)ptr;
 	printf("%s \n", message);
 	printf("thread done!\n");
 
@@ -9,6 +9,29 @@ void *t1(void *ptr){
 	while(1){
 		c = getche();
 		printf("\nYou typed: %c\n", c);
+		if(c == 'q') break;
+
+		switch(c){
+			case 't' :
+				snake_dir* = POSX;
+				break;
+			case 'y' :
+				snake_dir* = NEGX;
+				break;
+			case 'g' :
+				snake_dir* = POSY;
+				break;
+			case 'h' :
+				snake_dir* = NEGY;
+				break;
+			case 'i' :
+				snake_dir* = POSZ;
+				break;
+			case 'k' :
+				snake_dir* = NEGZ;
+				break;
+		}
+		printf("new direction is %d\n", snake_dir*);
 	}
 }
 
