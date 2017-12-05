43800158d39c7432f0660cb116d6a14c3248215d
Sarp Misoglu <sarp.misoglu@pomona.edu>
2017-12-04 23:26:02 -0800

    doesn't matter

--- squares/square_main.c
+++ squares/square_main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include "..\EasyPIO.h"
-#include "..\board_operations.h"
+#include "../EasyPIO.h"
+#include "../board_operations.h"
 
 
 
