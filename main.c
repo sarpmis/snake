46433a43fd0f6a0458444c310e61eb9deba5d827
Sarp Misoglu <sarp.misoglu@pomona.edu>
2017-12-04 17:30:51 -0800

    doesn't matter

--- main.c
+++ main.c
@@ -31,9 +31,9 @@ void main(void) {
 	int x = 0;
 	int z = 0;
 	int count = 0;
-	for(z; z < 8; z++){
-		for(x; x < 8; x++){
-			for(y; y < 8; y++){
+	for(z = 0; z < 8; z++){
+		for(x = 0; x < 8; x++){
+			for(y = 0; y < 8; y++){
 				count++;
 				printf("adding (%d,%d,%d)\n", x, y, z);
 				addToBoard(board, x, y, z);
