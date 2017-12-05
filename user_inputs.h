dee582b2fda5d202f177fcc775e26b534e13f802
Sarp Misoglu <sarp.misoglu@pomona.edu>
2017-12-04 19:33:56 -0800

    doesn't matter

--- user_inputs.h
+++ user_inputs.h
@@ -51,10 +51,12 @@ void speedSelect(int *gameSpeed){
 			case 's' :
 				*gameSpeed = 1000;
 				choosing = 0;
+				printf("Snail it is! \n");
 				break;
 			case 'd' :
 				*gameSpeed = 500;
 				choosing = 0;
+				printf("Dragon it is! \n")
 				break;
 			default :
 				printf("That's not a valid selection!\n");
